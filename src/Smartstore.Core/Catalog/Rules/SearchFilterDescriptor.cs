29060c684d50e78e400c77253428bc48fa1daf51
Aleksandar Apatović <alekap995@gmail.com>
2021-11-14 23:54:04 +0100

    fix

--- src/Smartstore.Core/Catalog/Rules/SearchFilterDescriptor.cs
+++ src/Smartstore.Core/Catalog/Rules/SearchFilterDescriptor.cs
@@ -12,7 +12,7 @@ namespace Smartstore.Core.Catalog.Rules
 
     public abstract class SearchFilterDescriptor : RuleDescriptor
     {
-        private SearchFilterDescriptor()
+        public SearchFilterDescriptor()
             : base(RuleScope.Product)
         {
         }
