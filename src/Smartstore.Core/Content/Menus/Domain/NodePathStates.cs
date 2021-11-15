472c71b980cc595cd4de896b33936f798656d2d7
Radoslav Ivic <r_ivic@hotmail.com>
2021-11-15 20:42:32 +0100

    Enumeration rename

--- src/Smartstore.Core/Content/Menus/Domain/NodePathStates.cs
+++ src/Smartstore.Core/Content/Menus/Domain/NodePathStates.cs
@@ -3,11 +3,11 @@
 namespace Smartstore.Core.Content.Menus
 {
     [Flags]
-    public enum NodePathState
+    public enum NodePathStates
     {
         None = 0,
         Parent = 1,
         Expanded = 2,
-        Selected = 3
+        Selected = 4
     }
 }
